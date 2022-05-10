54f2f803b6c470f418daae4e164ac72b92404c2f
Javi Sánchez <javier.sanchez.rois@gmail.com>
2022-05-10 15:33:18 +0200

    Underscores make numbers easier to read

--- src/main/kotlin/Gigasecond.kt
+++ src/main/kotlin/Gigasecond.kt
@@ -4,5 +4,5 @@ import java.time.LocalDateTime
 class Gigasecond(startDateTime: LocalDateTime) {
     constructor(startDate: LocalDate) : this(startDate.atStartOfDay())
 
-    val date: LocalDateTime = startDateTime.plusSeconds(1000000000)
+    val date: LocalDateTime = startDateTime.plusSeconds(1_000_000_000)
 }
